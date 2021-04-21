15c77c9123cdab8c6b727190f65931b21b406dbc
burned <burned@burned.finance>
2021-04-21 23:32:05 +0400

    :fire:(Token) Remove unused imports

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -2,10 +2,6 @@
 
 pragma solidity ^0.8.3;
 
-import "../access/Ownable.sol";
-//import "../interfaces/IUniswapV2Pair.sol";
-//import "../interfaces/IUniswapV2Factory.sol";
-//import "../interfaces/IUniswapV2Router02.sol";
 import "./ERC20.sol";
 
 contract Token is ERC20 {
@@ -22,21 +18,5 @@ contract Token is ERC20 {
         ERC20(name_, symbol_)
     {
         _mint(_msgSender(), 1_000_000 * 1E18);
-
-        //        IUniswapV2Router02 _uniswapV2Router =
-        //            IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
-        //        // Create a uniswap pair for this new token
-        //        uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
-        //            .createPair(address(this), _uniswapV2Router.WETH());
-        //
-        //        // set the rest of the contract variables
-        //        uniswapV2Router = _uniswapV2Router;
-
-        //exclude owner and this contract from fee
-
-        //        emit Transfer(address(0), _msgSender(), _tTotal);
     }
-
-    //to recieve ETH from uniswapV2Router when swaping
-    receive() external payable {}
 }
